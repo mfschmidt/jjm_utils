f45d09fd154db0d8fb916cb5e548e98728fe9a77
mfschmidt <mikeschmidt@schmidtgracen.com>
2021-06-25 23:57:48 -0400

    added scan of all /tmp/ dirs

--- pipelines/cleanup_tmp.sh
+++ pipelines/cleanup_tmp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+declare -a ACTIVE_JOBS
+N_ACTIVE=0
+
 JOBS=$(squeue | awk '{print $1}')
 for JOB in $JOBS; do
   if [[ "$JOB" != "JOBID" ]]; then
@@ -8,9 +12,31 @@ for JOB in $JOBS; do
     CMD=$(scontrol show job ${JOB} | grep Command)
     CMD=${CMD##*=}
     TS=${CMD%%.*}
-    TS=${TS##*_}    
+    TS=${TS##*_}
+    STAGING=$(ls -1d /tmp/*prep_${TS}_staging)
+    WORKING=$(ls -1d /tmp/*prep_${TS}_working)    
     if [[ "$HOST" == "$(hostname)" ]]; then
-    	echo "Job $JOB ran $CMD on host $HOST with timestamp $TS"
+    	echo "Host $HOST, Job $JOB, timestamp $TS:"
+    	echo "  C: $CMD"
+    	echo "  S: $STAGING"
+    	echo "  W: $WORKING"
+    	ACTIVE_JOBS[$TS]=$TS
+    	N_ACTIVE=$(( N_ACTIVE + 1 ))
+    fi
+  fi
+done
+
+echo "Found $N_ACTIVE active jobs on host $(hostname)."
+
+ALL_TMP=$(ls -1d /tmp/*prep_*_*ing)
+REGEX="_([0-9*])_"
+for TMP in ALL_TMP; do
+  if [[ $TMP =~ $REGEX ]]; then
+    TMP_TS="${BASH_REMATCH[1]}"
+    if [[ "${ARR[$TMP_TS]}" == "" ]]; then
+      echo "  OK to delete $TMP, inactive."
+    else
+      echo "  SAVE $TMP"
     fi
   fi
 done
