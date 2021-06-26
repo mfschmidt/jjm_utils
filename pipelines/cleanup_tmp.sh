840c1fdd1921637887f8ca5651f75a9039e7942c
mfschmidt <mikeschmidt@schmidtgracen.com>
2021-06-26 00:24:03 -0400

    removed extra prints from debugging

--- pipelines/cleanup_tmp.sh
+++ pipelines/cleanup_tmp.sh
@@ -31,16 +31,13 @@ echo "Found $N_ACTIVE active jobs on host $(hostname)."
 ALL_TMP=$(2>/dev/null ls -1d /tmp/*prep_*_*ing)
 REGEX=".*_([0-9]*)_.*ing"
 for TMP in $ALL_TMP; do
-  echo "  ...checking $TMP..."
   if [[ $TMP =~ $REGEX ]]; then
     TMP_TS="${BASH_REMATCH[1]}"
-    echo "   ... ${TMP_TS} vs ${ACTIVE_JOBS[TMP_TS]}"
     if [[ "${ACTIVE_JOBS[TMP_TS]}" == "" ]]; then
-      echo "  OK to delete $TMP, inactive."
+      echo "removing $(du -sh $TMP), job is finished."
+      echo "sudo rm -rf $TMP"
     else
-      echo "  SAVE $TMP"
+      echo "avoiding active job at $TMP"
     fi
-  else
-    echo "      no match"
   fi
 done
