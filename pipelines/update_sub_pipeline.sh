f605c812523aca5f6a5bf7c9c3d2189012bec603
mfschmidt <mikeschmidt@schmidtgracen.com>
2021-06-25 23:22:51 -0400

    made executable and included cleanup in script

--- pipelines/update_sub_pipeline.sh
+++ pipelines/update_sub_pipeline.sh
@@ -8,8 +8,8 @@
 ssh ${1} "
 cd ~/jjm_utils
 git pull
-sudo cp pipelines/sub_{pipeline,fmriprep,qsiprep} /usr/local/bin/
-sudo chown root:mriproc /usr/local/bin/sub_{pipeline,fmriprep,qsiprep}
-sudo chmod 755 /usr/local/bin/sub_{pipeline,fmriprep,qsiprep}
-ls -l /usr/local/bin/sub_{pipeline,fmriprep,qsiprep}
+sudo cp pipelines/sub_{pipeline,fmriprep,qsiprep} pipelines/cleanup_tmp.sh /usr/local/bin/
+sudo chown root:mriproc /usr/local/bin/sub_{pipeline,fmriprep,qsiprep} /usr/local/bin/cleanup_tmp.sh
+sudo chmod 755 /usr/local/bin/sub_{pipeline,fmriprep,qsiprep} /usr/local/bin/cleanup_tmp.sh
+ls -l /usr/local/bin/sub_{pipeline,fmriprep,qsiprep} /usr/local/bin/cleanup_tmp.sh
 "
