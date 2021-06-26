c26736aad536e9cf2d06d557a9c9a4248d76040e
mfschmidt <mikeschmidt@schmidtgracen.com>
2021-06-25 23:29:41 -0400

    added timestamp

--- pipelines/cleanup_tmp.sh
+++ pipelines/cleanup_tmp.sh
@@ -3,12 +3,14 @@
 JOBS=$(squeue | awk '{print $1}')
 for JOB in $JOBS; do
   if [[ "$JOB" != "JOBID" ]]; then
-    CMD=$(scontrol show job ${JOB} | grep Command)
-    CMD=${CMD##*=}
     HOST=$(scontrol show job ${JOB} | grep BatchHost)
     HOST=${HOST##*=}
+    CMD=$(scontrol show job ${JOB} | grep Command)
+    CMD=${CMD##*=}
+    TS=${CMD%%.*}
+    TS=${TS##*_}    
     if [[ "$HOST" == "$(hostname)" ]]; then
-    	echo "Job $JOB ran $CMD on host $HOST"
+    	echo "Job $JOB ran $CMD on host $HOST with timestamp $TS"
     fi
   fi
 done
