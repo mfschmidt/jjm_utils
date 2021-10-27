7728ae913f9d42c91351cc9776428b5b2b1b0751
mfschmidt <mikeschmidt@schmidtgracen.com>
2021-10-27 18:09:25 -0400

    add two confounds file filter scripts

---
+++ confounds/filter_confounds.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+# filter_confounds.py
+
+import os
+import pathlib
+import argparse
+import pandas as pd
+
+
+""" The first several functions return lists of columns for use
+    in filtering the dataframe.
+"""
+
+def motion_confounds(data, dof=6):
+    """ Return the specified number of motion regressor columns from data. """
+    
+    motion_prefixes = ["trans", "rot", ]
+    motion_axes = ["x", "y", "z", ]
+    motion_suffixes = ["derivative1", "power2", "derivative1_power2", ]
+    
+    column_candidates = []
+    if dof >= 6:
+        for pre in motion_prefixes:
+            for ax in motion_axes:
+                column_candidates.append(f"{pre}_{ax}")
+                if dof >= 9:
+                    column_candidates.append(f"{pre}_{ax}_{motion_suffixes[0]}")
+                if dof >= 12:
+                    column_candidates.append(f"{pre}_{ax}_{motion_suffixes[1]}")
+                if dof >= 15:
+                    column_candidates.append(f"{pre}_{ax}_{motion_suffixes[2]}")
+
+    return data[
+        [col for col in column_candidates if col in data.columns]
+    ]
+
+
+def basic_confounds(data):
+    """ Return only the basic regressor columns from data. """
+    column_candidates = ["framewise_displacement", ] +\
+        [col for col in data.columns if col.startswith("t_comp_cor_")] +\
+        [col for col in data.columns if col.startswith("a_comp_cor_")][:6]
+    return data[
+        [col for col in column_candidates if col in data.columns]
+    ]
+
+
+def curious_confounds(data):
+    """ Return the regressors we are interested in testing. """
+    column_candidates = ["global_signal", "csf", "white_matter", ]
+    return data[
+        [col for col in column_candidates if col in data.columns]
+    ]
+
+
+def scrubbed_confounds(data):
+    """ Return the regressors we are interested in testing. """
+    column_candidates = [
+        col for col in data.columns if col.startswith("motion_outlier")]
+    return data[
+        [col for col in column_candidates if col in data.columns]
+    ]
+
+
+def mod_confounds(args):
+    """ Read confounds and write out a subset, based on specified level. """
+
+    confounds_data = pd.read_csv(args.input, sep="\t", header=0)
+    full_shape = confounds_data.shape
+    
+    if args.level == "motion":
+        confounds_data = motion_confounds(confounds_data, args.motion)
+    elif args.level == "basic":
+        confounds_data = pd.concat([
+            basic_confounds(confounds_data),
+            motion_confounds(confounds_data, args.motion),
+        ], axis=1, )
+    elif args.level == "curious":
+        confounds_data = pd.concat([
+            basic_confounds(confounds_data),
+            motion_confounds(confounds_data, args.motion),
+            curious_confounds(confounds_data),
+        ], axis=1, )
+        
+    if args.scrub:
+        confounds_data = pd.concat([
+            confounds_data,
+            scrubbed_confounds(confounds_data),
+        ])
+    print("Read [{} TRs x {} regressors] confounds, writing [{} x {}]".format(
+        full_shape[0], full_shape[1],
+        confounds_data.shape[0], confounds_data.shape[1]
+    ))
+    if args.force or not os.path.isfile(args.output):
+        os.makedirs(pathlib.Path(args.output).parents[0], exist_ok=True)
+        confounds_data.to_csv(args.output, sep="\t", index=False)
+
+
+""" And finally, the interface components
+"""
+
+def get_arguments():
+    """ Parse command line arguments """
+    
+    parser = argparse.ArgumentParser(
+        description="Extract regressor columns from confounds file.",
+    )
+    parser.add_argument(
+        "-i", "--input",
+        help="Input, the complete confound file",
+    )
+    parser.add_argument(
+        "-o", "--output",
+        help="Output, the filtered regressor columns",
+    )
+    parser.add_argument(
+        "-l", "--level", default="basic",
+        help="A level label specifying which regressor columns to include"
+             "('motion', 'basic', 'curious')",
+    )
+    parser.add_argument(
+        "-m", "--motion", type=int, default=6,
+        help="How many motion degrees of freedom to regress (6, 9, 12, 15)",
+    )
+    parser.add_argument(
+        "-s", "--scrub", action="store_true",
+        help="Set to include motion outlier TRs as one-hot regressors",
+    )
+    parser.add_argument(
+        "-f", "--force", action="store_true",
+        help="Set to overwrite the output file, if it exists",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true",
+        help="Set to trigger verbose output",
+    )
+    
+    return parser.parse_args()
+    
+
+def main(args):
+    """ Entry point """
+
+    if args.verbose:
+        print(f"Reading {args.input} to write {args.output}.")
+        print(f"Extracting {args.level} with {args.motion} motion dof.")
+        
+    mod_confounds(args)
+
+
+if __name__ == "__main__":
+    main(get_arguments())
---
+++ confounds/filter_confounds.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# This script will read in the file specified as --input as a tab-separated table
+# It will filter the columns by the --subset name
+# And it will write out a new tab-separated file with only the columns needed.
+# Specify -v or --verbose for more descriptive output.
+
+# Handle command-line arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+  -i|--input)
+    INPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -o|--output)
+    OUTPUT_FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -s|--subset)
+    SUBSET="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -v|--verbose)
+    VERBOSE="True"
+    shift # past argument only
+    ;;
+  *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument only
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Fill in any missing arguments with defaults
+if [[ -z $INPUT_FILE ]]; then
+  echo "An input file must be specified."
+  exit 1
+fi
+if [[ -z $SUBSET ]]; then
+  SUBSET="motion6"
+fi
+if [[ -z $OUTPUT_FILE ]]; then
+  OUTPUT_FILE=${INPUT_FILE:: -4}
+  OUTPUT_FILE=${OUTPUT_FILE}_filter-${SUBSET}.tsv
+fi
+if [[ -f $OUTPUT_FILE ]]; then
+  echo "$OUTPUT_FILE exists and I don't want to overwrite it."
+  echo "Please delete it if you'd like to replace it."
+  echo "Quitting"
+  exit 1
+fi
+
+# Read columns available in input confounds file
+HEAD=$(head -1 $INPUT_FILE)
+
+# Define our criteria for columns to include/exclude
+declare -a T_COMP_CORS_ARRAY
+declare -a A_COMP_CORS_ARRAY
+BASIC_ARRAY=("framewise_displacement")
+MOTION_6_ARRAY=("trans_x" "trans_y" "trans_z" "rot_x" "rot_y" "rot_z")
+CURIOUS_ARRAY=("global_signal" "csf" "white_matter")
+for COL in $HEAD; do
+  if [[ "$COL" == "t_comp_cor_"* ]]; then
+    T_COMP_CORS_ARRAY+=("$COL")
+  fi
+  if [[ "$COL" == "a_comp_cor_"* ]]; then
+    if [[ "10#${COL:11}" -lt "5" ]]; then
+      A_COMP_CORS_ARRAY+=("$COL")
+    fi
+  fi
+done
+
+# Select columns from the confounds input file, based on the SUBSET
+declare -a COLS
+declare -a COL_IDS
+I=0
+for COL in $HEAD; do
+  (( I++ ))
+  if [[ " ${BASIC_ARRAY[@]} " =~ " ${COL} " ]]; then
+    if [[ "$SUBSET" == "basic" || "$SUBSET" == "curious" ]]; then
+      COLS+=("$COL")
+      COL_IDS+=("$I")
+    fi
+  fi
+  if [[ " ${MOTION_6_ARRAY[@]} " =~ " ${COL} " ]]; then
+    if [[ "$SUBSET" == "basic" || "$SUBSET" == "curious" || "$SUBSET" == "motion6" ]]; then
+      COLS+=("$COL")
+      COL_IDS+=("$I")
+    fi
+  fi
+  if [[ " ${T_COMP_CORS_ARRAY[@]} " =~ " ${COL} " ]]; then
+    if [[ "$SUBSET" == "basic" || "$SUBSET" == "curious" ]]; then
+      COLS+=("$COL")
+      COL_IDS+=("$I")
+    fi
+  fi
+  if [[ " ${A_COMP_CORS_ARRAY[@]} " =~ " ${COL} " ]]; then
+    if [[ "$SUBSET" == "basic" || "$SUBSET" == "curious" ]]; then
+      COLS+=("$COL")
+      COL_IDS+=("$I")
+    fi
+  fi
+  if [[ " ${CURIOUS_ARRAY[@]} " =~ " ${COL} " ]]; then
+    if [[ "$SUBSET" == "curious" ]]; then
+      COLS+=("$COL")
+      COL_IDS+=("$I")
+    fi
+  fi
+done
+
+# Do the actual filtering
+if [[ "${SUBSET}" == "all" ]]; then
+  cp $INPUT_FILE $OUTPUT_FILE
+else
+  AWK_COLS=""
+  for COL_ID in ${COL_IDS[@]}; do
+    if [[ "${#AWK_COLS}" -eq "0" ]]; then
+      AWK_COLS="\$${COL_ID}"
+    else
+      AWK_COLS="${AWK_COLS},\$${COL_ID}"
+    fi
+  done
+  awk -v FS='\t' -v OFS='\t' "{print $AWK_COLS}" $INPUT_FILE >> $OUTPUT_FILE
+fi
+
+# Alert the user to our interpretation
+if [[ "$VERBOSE" == "True" ]]; then
+  echo "INPUT FILE   = ${INPUT_FILE}"
+  echo "OUTPUT FILE  = ${OUTPUT_FILE}"
+  echo "SUBSET       = ${SUBSET}"
+  echo "     includes [${COLS[@]}]"
+  echo "Confounds ${SUBSET}-filtered and written to ${OUTPUT_FILE}"
+fi
+
