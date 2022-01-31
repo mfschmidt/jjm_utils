c37ce992ed18297578cd43159055446c7f75b97d
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2022-01-31 12:19:32 -0500

    moved limit options to end

--- ansible/full.sh
+++ ansible/full.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$1" != "" ]]; then
-  LIST="-l $1"
+  LIST="--limit $1"
 else
   LIST=""
 fi
 
 for SCRIPT in set_timezone initial_software install_docker install_slurm update_munge update_slurm; do
-  ansible-playbook ${LIST} ~/ansible/${SCRIPT}.yml
+  ansible-playbook ~/jjm_utils/ansible/${SCRIPT}.yml ${LIST}
 done
 
