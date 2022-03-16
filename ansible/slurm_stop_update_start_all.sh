3fa8e6c58be22da3ef6497e94acd852ab731d11a
mfschmidt <mikeschmidt@schmidtgracen.com>
2022-03-16 01:41:07 -0400

    heads and nodes

--- ansible/slurm_stop_update_start_all.sh
+++ ansible/slurm_stop_update_start_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # These scripts determine slurm controller and nodes from /etc/ansible/hosts
-# [slurm_head] and [slurm_nodes] groups, so do not use -l. Edit the hosts file to change
+# [heads] and [nodes] groups, so do not use -l. Edit the hosts file to change
 # which nodes are used.
 
-if [[ " $(cat /etc/ansible/hosts) " =~ slurm_head ]]; then
+if [[ " $(cat /etc/ansible/hosts) " =~ heads ]]; then
     echo "Updating and restarting slurm"
     ansible-playbook /home/aa/jjm_utils/ansible/stop_slurm.yml
     ansible-playbook /home/aa/jjm_utils/ansible/update_slurm.yml
     ansible-playbook /home/aa/jjm_utils/ansible/start_slurm.yml
 else
-    echo "Update /etc/ansible/hosts with 'slurm_head' node before updating"
+    echo "Update /etc/ansible/hosts with 'heads' node before updating"
 fi
 
