f5eaa6421b3aff864e9c77b61151681272e7e644
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2023-09-25 11:21:23 -0400

    load mask and weights in the same fxn

--- mri/decode.py
+++ mri/decode.py
@@ -78,6 +78,10 @@ def get_arguments():
         "--verbose", action="store_true",
         help="set to trigger verbose output",
     )
+    parser.add_argument(
+        "--debug", action="store_true",
+        help="set to trigger output of some extra values for comparisons",
+    )
 
     args = parser.parse_args()
 
@@ -115,8 +119,10 @@ def validated(args):
 
     # If a name was not provided, make a good guess
     if "name" not in args or args.name is None:
-        loc_underscore = args.decoder_mask.name.find("_mask")
-        setattr(args, "name", args.decoder_mask.name[0:loc_underscore])
+        loc_underscore = args.decoder_weights.name.find("_weights.")
+        if loc_underscore == -1:
+            loc_underscore = args.decoder_weights.name.find("_mask.")
+        setattr(args, "name", args.decoder_weights.name[0:loc_underscore])
         if args.verbose:
             print(f"detected '{args.name}' as decoder name")
     else:
@@ -129,7 +135,7 @@ def validated(args):
     return args
 
 
-def load_and_mask_bold(args):
+def load_and_mask_data(args):
     """ Load the 4D bold data, and mask it with the decoder mask. """
 
     bold_img = nib.load(args.bold_file)
@@ -140,6 +146,9 @@ def load_and_mask_bold(args):
     # If the x-scale in the affine has a different sign in either image,
     # the bold data needs to be flipped on the x-axis to match.
     # We anticipate this, using an LAS FSL decoder with RAS data.
+    # This is also very specific to our needs, matching LAS to RAS data
+    # that we know was registered to the same (barring order) templates.
+    # It IS NOT a universal registration algorithm!
     if mask_img.affine[0, 0] * bold_img.affine[0, 0] < 0.0:
         bold_4d_data = np.flip(bold_img.get_fdata(), axis=0)
         print(f"{red_on}"
@@ -165,7 +174,28 @@ def load_and_mask_bold(args):
         print(f"Masked BOLD data is shaped {masked_bold_data.shape} "
               f"and has {np.sum(masked_bold_data != 0.0):,} values.")
 
-    return masked_bold_data
+    if args.decoder_weights.name.endswith("nii.gz"):
+        # The mask contains weights within the volume, use them.
+        weight_img = nib.load(args.decoder_weights)
+        dims = weight_img.shape
+        voxels_per_volume = dims[0] * dims[1] * dims[2]
+        weight_2d_data = np.reshape(
+            weight_img.get_fdata(), voxels_per_volume, order="F"
+        )
+        # weights = weight_2d_data[mask_full_2d_data != 0]
+        # weights = weights.reshape((len(weights), 1))
+        # Add a bias, for the intercept. This is never zero or one in Noam's
+        # decoders, though. :( I am using a 0-intercept, and a brief
+        # investigation looked like putting it AFTER the data fits best.
+        weights = np.append(weight_2d_data[weight_2d_data != 0.0], 0.0)
+        # weights = np.insert(weight_2d_data[weight_2d_data != 0.0], 0, 0.0)
+        # double_mask = (weight_2d_data != 0.0) & (mask_full_2d_data != 0)
+    else:
+        weights = pd.read_csv(
+            args.decoder_weights, sep="\t", header=None
+        ).values
+
+    return masked_bold_data, weights
 
 
 def remove_motion(data, args, scale='zscore', method='manual'):
@@ -204,36 +234,18 @@ def remove_motion(data, args, scale='zscore', method='manual'):
         return motion_residuals
 
 
-def predict_y(data, args, null_weights=False):
+def predict_y(data, weights, args):
     """ Use measured BOLD data (cleaned) to predict y """
 
     # Normally, we use a decoder, which is a vector of weights.
     # But we may also want to use all ones for the decoder as a null comparison.
-    if null_weights:
+    if np.array_equal(np.ones(weights.shape), weights):
         words = "created", "as ones"
-        weights = np.ones((data.shape[0] + 1, 1))
     else:
         if args.decoder_weights.name.endswith('nii.gz'):
-            # The mask contains weights within the volume, use them.
             words = "extracted", "from decoder volume"
-            weight_img = nib.load(args.decoder_weights)
-            dims = weight_img.shape
-            voxels_per_volume = dims[0] * dims[1] * dims[2]
-            weight_2d_data = np.reshape(
-                weight_img.get_fdata(), voxels_per_volume, order='F'
-            )
-            # Add a bias, for the intercept. This is never zero or one in Noam's
-            # decoders, though. :( I am using a 0-intercept, and a brief
-            # investigation looked like putting it AFTER the data fits best.
-            # weights = np.append(weight_2d_data[weight_2d_data != 0.0], 0.0)
-            # weights = np.insert(weight_2d_data[weight_2d_data != 0.0], 0, 0.0)
-            weights = weight_2d_data[weight_2d_data != 0.0]
-            weights = weights.reshape((len(weights), 1))
         else:
             words = "loaded", "from weights vector"
-            weights = pd.read_csv(
-                args.decoder_weights, sep='\t', header=None
-            ).values
 
     if args.verbose:
         print(f"  {words[0]} {len(weights)} weights {words[1]}")
@@ -246,8 +258,30 @@ def predict_y(data, args, null_weights=False):
         x = np.append(data, np.ones((1, data.shape[1])), axis=0)
     y_hat = np.dot(weights.T, x).T
 
-    # This is the decoder score for each t, whatever it is the decoder is fit to
+    # This is the decoder score for each t
     return y_hat
+
+
+def write_some_matrices(data):
+    if len(data.shape) != 3:
+        print(f"not debugging {data.shape}-shaped matrix; expecting 3D.")
+    # Select a 3D patch that contains different mask labels
+    samp_x = 20  # int(data.shape[0] / 3)
+    samp_y = 72  # int(data.shape[1] * 2 / 3)
+    samp_z = 32  # int(data.shape[2] / 2)
+    for _z in range(samp_z, samp_z + 3):
+        print(
+            f"z slice {_z}; "
+            f"x = {samp_x} to {samp_x + 6}, "
+            f"y = {samp_y} to {samp_y + 4}:"
+        )
+        for _y in range(samp_y, samp_y + 5):
+            print(
+                f"y={_y:>3}:  " + ", ".join([
+                    f"{data[_x, _y, _z]:0.4f}"
+                    for _x in range(samp_x, samp_x + 7)
+                ])
+            )
 
 
 def main(args):
@@ -257,13 +291,27 @@ def main(args):
         str(args.bold_file), args.name
     ))
 
-    bold_data = load_and_mask_bold(args)
+    bold_data, weight_data = load_and_mask_data(args)
+    if args.verbose:
+        print(f"Shape of loaded data: {bold_data.shape}")
+        print(f"Shape of weights: {weight_data.shape}")
     final_data = remove_motion(bold_data, args, scale='zscore')
-    for null_weights in [True, False, ]:
-        label = "ones" if null_weights else "weights"
-        predicted_y = predict_y(final_data, args, null_weights=null_weights)
+    if args.verbose:
+        print(f"Shape of final data: {final_data.shape}")
+
+    if args.debug:
+        # Write out values from a specific region in each piece of data.
+        write_some_matrices(nib.load(args.bold_file).get_fdata()[:, :, :, 1])
+
+    for weights in [np.ones((weight_data.shape[0], 1)), weight_data, ]:
+        print(f"Shape of weights: {weights.shape}")
+        predicted_y = predict_y(final_data, weights, args)
         if np.sum(np.isnan(predicted_y)) > 0:
             print("NaN values in predicted y, no scores!")
+        if np.array_equal(np.ones(weights.shape), weights):
+            label = "ones"
+        else:
+            label = "weights"
         pd.DataFrame(predicted_y).to_csv(
             args.output_path / f"all_trs_{args.name}_{label}_scores.tsv",
             sep='\t', header=False, index=False,
